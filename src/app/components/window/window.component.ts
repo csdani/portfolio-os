415d5c3df0448623702c806f3ab4047d4cf914c9
csdani <13579TotalSense>
2025-05-31 20:03:42 +0200

    window component - drag behaviour, title bar

--- src/app/components/window/window.component.ts
+++ src/app/components/window/window.component.ts
@@ -1,16 +1,46 @@
-import { Component } from '@angular/core';
-import { CdkDrag } from '@angular/cdk/drag-drop';
+import { Component, Input } from '@angular/core';
+import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
 
 @Component({
     selector: 'app-window',
     standalone: true,
-    imports: [CdkDrag],
+    imports: [CdkDrag, CdkDragHandle],
     templateUrl: './window.component.html',
     styleUrl: './window.component.scss'
 })
 export class WindowComponent {
+    @Input() title: string = 'Window Title';
+
+    dragActive = false;
+
+    fullscreenWindow() {
+        console.log('Window fullscreene clicked');
+    }
+    minimalizeWindows() {
+        console.log('Window minimize clicked');
+    }
     closeWindow() {
-        console.log('Window closed');
+        console.log('Window close clicked');
+    }
+
+    onTitleBarMouseDown(event: MouseEvent) {
+        const buttons = (event.currentTarget as HTMLElement).querySelector('.window-buttons-container');
+        if (buttons && buttons.contains(event.target as Node)) {
+            return;
+        }
+        this.activeDrag();
+    }
+
+    onTitleBarMouseUp($event: MouseEvent) {
+        this.deactiveDrag();
+    }
+
+    private activeDrag() {
+        this.dragActive = true;
+    }
+
+    private deactiveDrag() {
+        this.dragActive = false;
     }
 
 }
